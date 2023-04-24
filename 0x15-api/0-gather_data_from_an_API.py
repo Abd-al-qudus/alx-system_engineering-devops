1eeefcf1423acb4d95cf2486124090b91d7cac4b
Abd-al-qudus <jimohqudus86@gmail.com>
2023-04-24 23:18:00 +0100

    python api with requests

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''This scripts gets an employee's todo list
+   and return it using REST API'''
+from sys import argv
+import requests as req
+
+if __name__ == '__main__':
+    if len(argv) != 2:
+        print('Usage: script <employee id>')
+    else:
+        id = argv[1]
+        url = 'https://jsonplaceholder.typicode.com/users/{}/todos/'.format(id)
+        user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
+        resp = req.get(url).json()
+        user_details = req.get(user_url).json()
+        completed = [userDict for userDict in resp
+                     if userDict.get('completed')]
+        print('Employer {} is done with tasks({}/{}):'
+              .format(user_details.get('name'), len(completed), len(resp)))
+        for user in completed:
+            print(' \t{}'.format(user.get('title')))
