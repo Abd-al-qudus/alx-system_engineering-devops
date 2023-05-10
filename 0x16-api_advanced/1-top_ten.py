e8ed85b4a83566a262f6220d6573da5d4a0369ce
Abd-al-qudus <jimohqudus86@gmail.com>
2023-05-10 10:27:12 +0100

    fixed output error for top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,10 +10,14 @@ import requests
 def top_ten(subreddit):
     """takes a subreddit and print top ten post"""
     if not isinstance(subreddit, str) or subreddit is None:
-        return 0
+        return None
     headers = {'User-Agent': 'Engineer-Phoenix-0x16-advanced-API'}
     API = "https://www.reddit.com/r/{}.json".format(subreddit)
-    req = requests.get(API, allow_redirects=False, headers=headers).json()
-    responses = req.get('data', {}).get('children', [])[:10]
-    [print(response.get('data', {}).get('title', None))
-        for response in responses]
+    req = requests.get(API, allow_redirects=False, headers=headers,
+                       params={'limit': 10}).json()
+    responses = req.get('data', {}).get('children', [])
+    if responses:
+        [print(response.get('data').get('title'))
+            for response in responses]
+    else:
+        print("None")
