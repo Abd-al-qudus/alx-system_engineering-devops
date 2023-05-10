7d3e93fe948e2c8af299081e0d9f68be3198e003
Abd-al-qudus <jimohqudus86@gmail.com>
2023-05-10 09:39:53 +0100

    sysadmin api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This script queries the reddit api and
+    return the number of current users, returns 0
+    if the subreddit is invalid
+    Author : Engineer Phoenix
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """takes a subreddit and return the total subscribers"""
+    if not isinstance(subreddit, str) or subreddit is None:
+        return 0
+    headers = {'User-Agent': 'Engineer-Phoenix-0x16-advanced-API'}
+    API = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    req = requests.get(API, allow_redirects=False, headers=headers).json()
+    return req.get('data').get('subscribers')
