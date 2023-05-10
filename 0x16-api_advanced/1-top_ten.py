b43a2eb11eaa17a43a15d9726fcc340675d4aeb2
Abd-al-qudus <jimohqudus86@gmail.com>
2023-05-10 10:07:42 +0100

    top ten post on reddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This script queries the reddit api and
+    print the first ten posts of the subreddit
+    returns 0 if the subreddit is invalid
+    Author : Engineer Phoenix
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """takes a subreddit and print top ten post"""
+    if not isinstance(subreddit, str) or subreddit is None:
+        return 0
+    headers = {'User-Agent': 'Engineer-Phoenix-0x16-advanced-API'}
+    API = "https://www.reddit.com/r/{}.json".format(subreddit)
+    req = requests.get(API, allow_redirects=False, headers=headers).json()
+    responses = req.get('data', {}).get('children', [])[:10]
+    [print(response.get('data', {}).get('title', "")) for response in responses]
